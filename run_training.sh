42f442e3ede5ca79115dd766e8a82dcb93cbc040
Zhijian Mei <545902915@qq.com>
2023-03-14 23:01:20 +0800

    Update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0,1 python main.py \
     --model models/rationale \
     --user_msg rationale --img_type detr \
     --data_root ../../FolkScope \
-    --bs 8 --eval_bs 4 --eval_acc 10 --output_len 512 \
+    --bs 4 --eval_bs 4 --eval_acc 10 --output_len 512 \
     --final_eval --prompt_format QCM-LE --output_dir test
 
 ## answer inference
