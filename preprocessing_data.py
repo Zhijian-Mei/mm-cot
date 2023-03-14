977325e43569572253ed1dca3d90c73d9489b037
Zhijian Mei <545902915@qq.com>
2023-03-14 17:58:52 +0800

    Update preprocessing_data.py

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -6,8 +6,7 @@ from tqdm import trange
 
 df = pd.read_csv(os.path.join('../../FolkScope', 'TOTAL_typicality_result.csv'))
 img_features = np.load('vision_features/amazon_img_detr.npy',allow_pickle=True).item()
-print(img_features['B01HIA5MF2_1'])
-quit()
+
 source_text = []
 target_text = []
 img_id_as = []
